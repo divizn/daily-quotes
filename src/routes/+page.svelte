0382fd8e6fec25546e3982aff15777e5193d1e70
phon <hassanjaved186@gmail.com>
2024-10-06 11:36:54 +0100

    chore: use more idiomatic approach for accessing store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,12 @@
 <script lang="ts">
     import Quote from '$lib/components/Quote.svelte';
 	import quoteStore from '$lib/stores/quoteStore';
-	import type { Quote as TQuote } from '$lib/stores/quoteStore';
 
-	let quote: TQuote | null = null;
-
-	quoteStore.subscribe((quotes: TQuote[]) => {
-		quote = quotes[1] || null; // Set the current quote to the first one
-	});
-    console.log('Hello from +page.svelte');
+    let quote = $quoteStore[0];
 </script>
 
 <main class="flex-grow flex items-center justify-center">
-    {#if quote}
+    {#if $quoteStore}
         <Quote {quote}/>
     {:else}
         <p>Loading...</p>
