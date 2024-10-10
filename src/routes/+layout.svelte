18160865b76e44657cae6f7560d1c63b18dcebf0
phon <hassanjaved186@gmail.com>
2024-10-10 05:05:38 +0100

    feat: vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css';
 	import Header from '$lib/components/ui/Header.svelte';
 	import { ModeWatcher } from 'mode-watcher';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="flex flex-col min-h-screen">
