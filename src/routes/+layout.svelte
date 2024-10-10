f30ffb66be0303ee53cc59f11a67eeb45a54e8f8
phon <hassanjaved186@gmail.com>
2024-10-10 06:22:21 +0100

    chore: remove vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,6 @@
 	import '../app.css';
 	import Header from '$lib/components/ui/Header.svelte';
 	import { ModeWatcher } from 'mode-watcher';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		inject({ mode: dev ? 'development' : 'production' });
-	});
 </script>
 
 <div class="flex flex-col min-h-screen">
