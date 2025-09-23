5cda58385529983f65b5c2ea18640410c53af70e
phon <hassanjaved186@gmail.com>
2025-09-23 19:19:16 +0100

    chore: fix padding so text doesnt clip

--- src/lib/components/Quote.svelte
+++ src/lib/components/Quote.svelte
@@ -6,7 +6,7 @@
   
 <Card class="sm:w-4/5 h-max sm:mx-auto -mt-2 border-0 sm:mt-10">
   <CardContent class="space-y-4">
-    <blockquote class="text-3xl sm:text-6xl font-semibold italic text-transparent bg-gradient-to-t from-purple-500 to-blue-600 bg-clip-text dark:from-blue-400 dark:to-purple-600">
+    <blockquote class="text-3xl sm:text-6xl font-semibold italic p-3 text-transparent bg-gradient-to-t from-purple-500 to-blue-600 bg-clip-text dark:from-blue-400 dark:to-purple-600">
       "{quote.text}"
     </blockquote>
     <p class="text-lg sm:text-xl text-zinc-700 dark:text-white text-right font-mono">
